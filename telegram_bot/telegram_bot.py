7c6f79d576e9bdee96b207335449f8abf12c5c0b
Miha Donzhuk <michael01@bk.ru>
2024-11-12 16:10:31 +0300

    Ver_9.2

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,8 +1,6 @@
 import telebot
 import config
 
-# from landing_page.models import UserContacts
-
 bot = telebot.TeleBot(config.bot_token)
 chat_id = config.my_id
 
@@ -14,8 +12,3 @@ def send_contact_to_telegram(name, phone_number):
     bot.send_message(chat_id, f"<strong>Новый контакт</strong>\nИмя: <b>{name}</b>\nНомер: <b>{phone_number}</b>", parse_mode='html')
 
 
-
-
-# @bot.message_handler(commands=[])
-# def send_contact(name, phone_number):
-#     bot.send_message(chat_id, f'{name}, {phone_number}')
